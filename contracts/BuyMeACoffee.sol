dbea96557287ce6dbea8b11fdacecd5878ae3518
Dhaesha M <dhaesha1@gmail.com>
2022-05-13 22:48:39 -0400

    added state variables

--- contracts/BuyMeACoffee.sol
+++ contracts/BuyMeACoffee.sol
@@ -6,7 +6,11 @@ import "hardhat/console.sol";
 contract BuyMeACoffee {
 // event to emit when memo is created
 event NewMemo() {
-    
+    // indexed adds counter to query
+    address indexed from,
+    uint256 timestamp,
+    string name,
+    string message,
 }  
 
 // memo struck 
@@ -16,4 +20,10 @@ struct Memo {
     string name;
     string message;
 }
+
+// list of all memos recieved
+Memo[] memos;
+
+// address of contract deployer  (for when its time to collect tips)
+address payable owner;
 }

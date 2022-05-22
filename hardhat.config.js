237ac0907293869d769ccd03ad18fc3e2ca23db2
Dhaesha M <dhaesha1@gmail.com>
2022-05-22 17:14:44 -0400

    smart contract deployed to goerli testnet at 0x5369B7310407904cF02Cf50bFcC42D6E41284810 using Alchemy Dashboard

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,14 +1,19 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+require("dotenv").config();
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+const GOERLI_URL = process.env.GOERLI_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
+// // This is a sample Hardhat task. To learn how to create your own go to
+// // https://hardhat.org/guides/create-task.html
+// task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+//   const accounts = await hre.ethers.getSigners();
+
+//   for (const account of accounts) {
+//     console.log(account.address);
+//   }
+// });
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -18,4 +23,10 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    goerli: {
+      url: GOERLI_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,7 +5,7 @@ async function main() {
   const BuyMeACoffee = await hre.ethers.getContractFactory("BuyMeACoffee");
   const buyMeACoffee = await BuyMeACoffee.deploy();
   await buyMeACoffee.deployed();
-  // console.log("BuyMeACoffee deployed to :  ", buyMeACoffee.address);
+  console.log("BuyMeACoffee deployed to :  ", buyMeACoffee.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
